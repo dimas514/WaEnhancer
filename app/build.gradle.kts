efe69688bbd33306e879b292d4497e88c4502beb
dimas514 <66823902+dimas514@users.noreply.github.com>
2026-03-17 02:40:47 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,9 +74,7 @@ android {
 
         ndk {
             abiFilters.add("armeabi-v7a")
-            abiFilters.add("arm64-v8a")
-            abiFilters.add("x86_64")
-            abiFilters.add("x86")
+            
         }
 
     }
